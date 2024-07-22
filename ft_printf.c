885e65efc19ceafb8ce7ebe6d1a6c1038780312e
fmaurer <fmaurer42@gmail.com>
2024-07-22 11:41:31 +0200

    refac: rename printed chars variable

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: fmaurer <fmaurer42@posteo.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 11:43:28 by fmaurer           #+#    #+#             */
-/*   Updated: 2024/07/21 08:03:20 by fmaurer          ###   ########.fr       */
+/*   Updated: 2024/07/22 11:39:58 by fmaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,16 @@
 int	ft_printf(const char *fmt, ...)
 {
 	va_list	args;
-	int		printed;
+	int		chars_printed;
 
 	if (!fmt || !ftpr_check_fmt(fmt))
 		return (-1);
 	if (!*fmt)
 		return (0);
 	va_start(args, fmt);
-	printed = ftpr_parse_args(args, fmt);
+	chars_printed = ftpr_parse_args(args, fmt);
 	va_end(args);
-	return (printed);
+	return (chars_printed);
 }
 
 #ifndef BONUS
